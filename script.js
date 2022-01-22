3b89230c3ac156e7a5a4ed496fcab5a363641412
adelkaamarenka <adelka.dilaj@gmail.com>
2022-01-23 00:36:14 +0100

    finished

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minutesEl = document.getElementById("minutes");
+const secondsEl = document.getElementById("seconds");
+
 function countdown() {
-  const newYearsDate = new Date(2023, 00, 01);
+  const newYearsDate = new Date("1 Jan 2023");
   const currentDate = new Date();
-  const seconds = (newYearsDate - currentDate) / 1000;
-  const days = Math.floor(seconds / 3600 / 24);
-  console.log(days);
+
+  const totalSeconds = (newYearsDate - currentDate) / 1000;
+
+  const days = Math.floor(totalSeconds / 3600 / 24);
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const minutes = Math.floor(totalSeconds / 60) % 60;
+  const seconds = Math.floor(totalSeconds % 60);
+
+  daysEl.innerHTML = days;
+  hoursEl.innerHTML = hours;
+  minutesEl.innerHTML = minutes;
+  secondsEl.innerHTML = seconds;
 }
 
 countdown();
+setInterval(countdown, 1000);
