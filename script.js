a6a92f0e50e055ea8da9537c5521c09512a740f8
adelkaamarenka <adelka.dilaj@gmail.com>
2022-01-23 00:13:22 +0100

    cleaned up countdown

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 function countdown() {
   const newYearsDate = new Date(2023, 00, 01);
-  console.log(newYearsDate);
   const currentDate = new Date();
-  console.log(currentDate);
-  const seconds = (newYearsDate.getTime() - currentDate.getTime()) / 1000;
+  const seconds = (newYearsDate - currentDate) / 1000;
   const days = Math.floor(seconds / 3600 / 24);
   console.log(days);
 }
