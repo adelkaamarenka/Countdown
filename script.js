afc7216bffb9cdaf0cc049781038e5f830e5f470
adelkaamarenka <adelka.dilaj@gmail.com>
2022-01-23 00:11:25 +0100

    fixed countdown

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function countdown() {
-  const newYearsDate = new Date(2023, 01, 01);
+  const newYearsDate = new Date(2023, 00, 01);
+  console.log(newYearsDate);
   const currentDate = new Date();
-  const seconds = (newYearsDate - currentDate) / 1000;
+  console.log(currentDate);
+  const seconds = (newYearsDate.getTime() - currentDate.getTime()) / 1000;
   const days = Math.floor(seconds / 3600 / 24);
-  console.log(days - 30);
+  console.log(days);
 }
 
 countdown();
